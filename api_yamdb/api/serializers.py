a5001faeb801e5524f104f8dfe5d99901fd898d4
Kristina Alabova <kristina.alabova@mail.ru>
2025-04-01 18:59:03 +0300

    Added models, views, urls, serializers for Title, Category, Genre

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,66 @@
+from django.utils import timezone
+from rest_framework import serializers
+# from rest_framework.validators import UniqueTogetherValidator
+
+from reviews.models import Title, Genre, Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Category."""
+
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Genre."""
+
+    class Meta:
+        model = Genre
+        exclude = ('id',)
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    """Базовый сериализатор для модели Title."""
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            # 'rating',
+            'description',
+            'genre',
+            'category'
+        )
+
+    def validate_year(self, value):
+        """Проверка года создания произведения."""
+        current_year = timezone.now().year
+        if value > current_year:
+            raise serializers.ValidationError(
+                'Год создания не может быть больше текущего!',
+            )
+        return value
+
+
+class TitleReadSerializer(TitleSerializer):
+    """Сериализатор Title для чтения."""
+    genre = GenreSerializer(read_only=True, many=True)
+    category = CategorySerializer(read_only=True)
+
+
+class TitleWriteSerializer(TitleSerializer):
+    """Сериализатор Title для записи."""
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all(),
+        required=False,
+    )
+    genre = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Genre.objects.all(),
+        many=True,
+    )
